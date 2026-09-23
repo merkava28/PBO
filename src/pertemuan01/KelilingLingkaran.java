6de33153947fa6c3fd137357ccd1bedfdee11754
merkava28 <awira5746@gmail.com>
2026-09-23 11:04:26 +0700

    update keliling lingkaran

--- src/pertemuan01/KelilingLingkaran.java
+++ src/pertemuan01/KelilingLingkaran.java
@@ -6,18 +6,17 @@ public class KelilingLingkaran {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
 
-        // Input jari-jari
-        System.out.print("Masukkan jari-jari lingkaran: ");
-        double jariJari = input.nextDouble();
+            // Input jari-jari
+            System.out.print("Masukkan jari-jari lingkaran: ");
+            double jariJari = input.nextDouble();
 
-        // Menghitung keliling
-        double keliling = 2 * Math.PI * jariJari;
+            // Menghitung keliling
+            double keliling = 2 * Math.PI * jariJari;
 
-        // Menampilkan hasil
-        System.out.println("Keliling lingkaran = " + keliling);
-
-        input.close();
+            // Menampilkan hasil
+            System.out.println("Keliling lingkaran = " + keliling);
+        }
     }
 }
